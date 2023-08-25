1ec66d56a92286d664a9ef3a41ebdf6efc23b0d9
brycebien <brycebien@gmail.com>
2023-08-25 12:26:56 -0400

    first commit

---
+++ individual-page.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+    <title>Bryce Bien's Resume</title>
+    
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Your name's resume">
+    <meta name="author" content="Your name">    
+    <link rel="shortcut icon" href="favicon.ico"> 
+    
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+    
+    <!-- FontAwesome JS-->
+	<script defer src="assets/fontawesome/js/all.min.js"></script>
+       
+    <!-- Theme CSS -->  
+    <link id="theme-style" rel="stylesheet" href="assets/css/pillar-1.css">
+
+
+</head> 
+
+<body>
+    <article class="resume-wrapper text-center position-relative">
+	    <div class="resume-wrapper-inner mx-auto text-start bg-white shadow-lg">
+		    <header class="resume-header pt-4 pt-md-0">
+			    <div class="row">
+				    <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
+				        <img class="picture" src="assets/images/ProfileImg.jpg" height="220px" alt="">
+				    </div><!--//col-->
+				    <div class="col">
+					    <div class="row p-4 justify-content-center justify-content-md-between">
+						    <div class="primary-info col-auto">
+							    <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase"><?php echo 'Bryce Bien';?></h1>
+							    <div class="title mb-3"><?php echo 'Software Engineer';?></div>
+							    <ul class="list-unstyled">
+								    <li class="mb-2"><a class="text-link" href="mailto:brycebien@gmail.com"><i class="far fa-envelope fa-fw me-2" data-fa-transform="grow-3"></i><?php echo 'brycebien@gmail.com';?></a></li>
+								    <li><a class="text-link" href="#"><i class="fas fa-mobile-alt fa-fw me-2" data-fa-transform="grow-6"></i><?php echo '(513)-283-2332';?></a></li>
+							    </ul>
+						    </div><!--//primary-info-->
+						    <div class="secondary-info col-auto mt-2">
+							    <ul class="resume-social list-unstyled">
+					                <li class="mb-3"><a class="text-link" href="https://www.linkedin.com/in/brycebien/"><span class="fa-container text-center me-2"><i class="fab fa-linkedin-in fa-fw"></i></span><?php echo 'linkedin.com/in/brycebien';?>/</a></li>
+					                <li class="mb-3"><a class="text-link" href="https://github.com/brycebien"><span class="fa-container text-center me-2"><i class="fab fa-github-alt fa-fw"></i></span><?php echo 'github.com/brycebien';?></a></li>
+					                <li><a class="text-link" href="https://brycebien.github.io/MyWebsite/"><span class="fa-container text-center me-2"><i class="fas fa-globe"></i></span><?php echo 'brycebien.github.io/MyWebsite/';?></a></li>
+							    </ul>
+						    </div><!--//secondary-info-->
+					    </div><!--//row-->
+					    
+				    </div><!--//col-->
+			    </div><!--//row-->
+		    </header>
+		    <div class="resume-body p-5">
+			    <section class="resume-section summary-section mb-5">
+				    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo 'Summary';?></h2>
+				    <div class="resume-section-content">
+					    <p class="mb-0">
+							<?php echo 'I am currently a junior studying Applied Software Engineering and Business at Northern Kentucky University. Thus far I have completed Object Oriented Programming I and II, Full-Stack Application Development, and Database Programming. As of the Fall 2023 semester I am enrolled in Cross-Platform Development, Human-Computer Interaction, Server-Side Programming, and Software Design.';?>
+						</p>
+				    </div>
+			    </section><!--//summary-section-->
+			    <div class="row">
+				    <div class="col-lg-9">
+					    <section class="resume-section experience-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo 'Work Experience';?></h2>
+						    <div class="resume-section-content">
+							    <div class="resume-timeline position-relative">
+								    
+								    <article class="resume-timeline-item position-relative pb-5">
+									    
+									    <div class="resume-timeline-item-header mb-2">
+										    <div class="d-flex flex-column flex-md-row">
+										        <h3 class="resume-position-title font-weight-bold mb-1"><?php echo 'Shift Lead';?></h3>
+										        <div class="resume-company-name ms-auto"><?php echo 'United Dairy Farmers';?></div>
+										    </div><!--//row-->
+										    <div class="resume-position-time"><?php echo '2022 - present';?></div>
+									    </div><!--//resume-timeline-item-header-->
+									    <div class="resume-timeline-item-desc">
+										    <p>
+												<?php echo 'Assist customers in ordering ice cream and checking out. Work in a coordinated team to complete orders in a timely manner.';?>	
+											</p>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold"><?php echo 'Achievements';?></h4>
+										    <p>
+												<?php echo 'Gained valuable customer service skills as well as more expeirence working as a team to complete tasks.';?>	
+											</p>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold"><?php echo 'Technologies used:';?></h4>
+										    <ul class="list-inline">
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo 'Customer Service Skills';?></span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo 'Team Management Skills';?></span></li>
+										    </ul>
+									    </div><!--//resume-timeline-item-desc-->
+
+								    </article><!--//resume-timeline-item-->
+								    
+								    <article class="resume-timeline-item position-relative pb-5">
+									    
+									    <div class="resume-timeline-item-header mb-2">
+										    <div class="d-flex flex-column flex-md-row">
+										        <h3 class="resume-position-title font-weight-bold mb-1"><?php echo 'Lead Line Cook';?></h3>
+										        <div class="resume-company-name ms-auto"><?php echo 'Logan Supermart';?></div>
+										    </div><!--//row-->
+										    <div class="resume-position-time"><?php echo '2019 - 2022';?></div>
+									    </div><!--//resume-timeline-item-header-->
+									    <div class="resume-timeline-item-desc">
+										    <p><?php echo 'Assisted customers with ordering and checking out.';?></p>
+										    <p><?php echo 'Coordinated a team to complete orders on time by directing them on what to do.';?></p>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold"><?php echo 'Technologies used:';?></h4>
+										    <ul class="list-inline">
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo 'Blacktop Grill';?></span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo 'Fryer';?></span></li>
+										    </ul>
+									    </div><!--//resume-timeline-item-desc-->
+
+								    </article><!--//resume-timeline-item-->
+								    
+								    <article class="resume-timeline-item position-relative">
+									    
+									    <div class="resume-timeline-item-header mb-2">
+										    <div class="d-flex flex-column flex-md-row">
+										        <h3 class="resume-position-title font-weight-bold mb-1"><?php echo 'Ski Technition';?> <small class="text-muted"></small></h3>
+										        <div class="resume-company-name ms-auto"><?php echo 'Perfect North Slopes';?></div>
+										    </div><!--//row-->
+										    <div class="resume-position-time"><?php echo '2016 - 2020';?></div>
+									    </div><!--//resume-timeline-item-header-->
+									    <div class="resume-timeline-item-desc">
+										    <p>
+												<?php echo 'Assisted customers with navigating the rental shop by directing them where to go and assisting
+												with putting on boots as well as assigning skis and snowboards to patrons.
+												Tuned, mounted, and waxed skis and snowboards.';?>	
+											</p>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold"><?php echo 'Technologies used:';?></h4>
+										    <ul class="list-inline">
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo 'Edge Grinding Machine';?></span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo 'Belt Sander';?></span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo 'Drills';?></span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo 'Wax & Iron';?></span></li>
+										    </ul>
+									    </div><!--//resume-timeline-item-desc-->
+
+								    </article><!--//resume-timeline-item-->
+								    
+								    
+							    </div><!--//resume-timeline-->
+							    
+							    
+							    
+							    
+							    
+							    
+						    </div>
+					    </section><!--//projects-section-->
+				    </div>
+				    <div class="col-lg-3">
+					    <section class="resume-section skills-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo 'Skills';?> &amp; <?php echo 'Tools';?></h2>
+						    <div class="resume-section-content">
+						        <div class="resume-skill-item">
+							        <ul class="list-unstyled mb-4">
+								        <li class="mb-2">
+								            <div class="resume-skill-name"><?php echo 'Java';?></div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 90%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name"><?php echo 'Dart/Flutter';?></div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 0%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name"><?php echo 'JavaScript';?></div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 98%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        
+								        <li class="mb-2">
+								            <div class="resume-skill-name"><?php echo 'Node.js';?></div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 98%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name"><?php echo 'HTML/CSS';?></div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 98%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+							        </ul>
+						        </div><!--//resume-skill-item-->
+						        <div class="resume-skill-item">
+						            <h4 class="resume-skills-cat font-weight-bold"><?php echo 'Others';?></h4>
+						            <ul class="list-inline">
+							            <li class="list-inline-item"><span class="badge badge-light"><?php echo 'Git';?></span></li>
+										<li class="list-inline-item"><span class="badge badge-light"><?php echo 'Wireframing';?></span></li>
+										<li class="list-inline-item"><span class="badge badge-light"><?php echo 'Python';?></span></li>
+										<li class="list-inline-item"><span class="badge badge-light"><?php echo 'MySQL';?></span></li>
+										<li class="list-inline-item"><span class="badge badge-light"><?php echo 'Team Leadership';?></span></li>
+										<li class="list-inline-item"><span class="badge badge-light"><?php echo 'Teamwork';?></span></li>
+										<li class="list-inline-item"><span class="badge badge-light"><?php echo 'Customer Service';?></span></li>
+						            </ul>
+						        </div><!--//resume-skill-item-->
+						    </div><!--resume-section-content-->
+					    </section><!--//skills-section-->
+					    <section class="resume-section education-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo 'Education';?></h2>
+						    <div class="resume-section-content">
+							    <ul class="list-unstyled">
+								    <li class="mb-2">
+								        <div class="resume-degree font-weight-bold"><?php echo 'BS in Applied Software Engenerring';?></div>
+								        <div class="resume-degree-org"><?php echo 'Northern Kentucky University';?></div>
+								        <div class="resume-degree-time"><?php echo '2021 - 2025';?></div>
+								    </li>
+								    <li>
+								        <div class="resume-degree font-weight-bold"><?php echo 'Minor in Business';?></div>
+								        <div class="resume-degree-org"><?php echo 'Northern Kentucky University';?></div>
+								        <div class="resume-degree-time"><?php echo '2021 - 2025';?></div>
+								    </li>
+							    </ul>
+						    </div>
+					    </section><!--//education-section-->
+					    <section class="resume-section reference-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo 'Awards';?></h2>
+						    <div class="resume-section-content">
+							    <ul class="list-unstyled resume-awards-list">
+								    <li class="mb-2 ps-4 position-relative">
+								        <i class="resume-award-icon fas fa-trophy position-absolute" data-fa-transform="shrink-2"></i>
+								        <div class="resume-award-name"><?php echo 'Outstanding Academic Achievement';?></div>
+								        <div class="resume-award-desc"><?php echo 'Dean\'s List Spring 2023 Semester at Northern Kentucky University';?></div>
+								    </li> 
+							    </ul>
+						    </div>
+					    </section><!--//interests-section-->
+					    <section class="resume-section language-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo 'Languages';?></h2>
+						    <div class="resume-section-content">
+							    <ul class="list-unstyled resume-lang-list">
+								    <li class="mb-2"><span class="resume-lang-name font-weight-bold"><?php echo 'English';?></span> <small class="text-muted font-weight-normal"><?php echo '(Native)';?></small></li>
+							    </ul>
+						    </div>
+					    </section><!--//language-section-->
+					    <section class="resume-section interests-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo 'Interests';?></h2>
+						    <div class="resume-section-content">
+							    <ul class="list-unstyled">
+								    <li class="mb-1"><?php echo 'Climbing';?></li>
+								    <li class="mb-1"><?php echo 'Snowboarding';?></li>
+								    <li class="mb-1"><?php echo 'Camping';?></li>
+							    </ul>
+						    </div>
+					    </section><!--//interests-section-->
+					    
+				    </div>
+			    </div><!--//row-->
+				<section class="resume-section experience-section mb-5">
+					<h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo 'Projects';?></h2>
+					<div class="row mt-4">
+						<div class="col-md-4">
+							<div class="card">
+								<img src="assets\images\Project1.jpg" alt="Project 1" class="card-img-top">
+								<div class="card-body">
+									<h5 class="card-title"><?php echo 'Project 1';?></h5>
+									<p class="card-text"><?php echo 'Final Assignment for Full-Stack Application Development. Created a concert review site with a team of two others.';?></p>
+									<a href="https://github.com/AndrewOneal/MidwestMusic" href="btn btn-outline-primary"><?php echo 'Go to link';?></a>
+								</div>
+							</div>
+						</div>
+						
+					</div>
+				</section><!--//projects-section-->
+		    </div><!--//resume-body-->
+		    
+		    
+	    </div>
+    </article> 
+
+    
+    <footer class="footer text-center pt-2 pb-5">
+	    <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
+        <small class="copyright"><?php echo 'Designed with';?> <span class="sr-only"><?php echo 'love';?></span><i class="fas fa-heart"></i> <?php echo 'by Bryce Bien';?></small>
+    </footer>
+
+    
+
+</body>
+</html> 
+
